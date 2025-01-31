e5e0a1462aca75b4a834da70092d7ad0b57557b1
geramalagon <60306762+geramalagon@users.noreply.github.com>
2025-01-31 13:07:50 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out', // Specify the output directory
   images: { 
     unoptimized: true,
     remotePatterns: [
